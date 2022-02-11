0eae7ec1d343339e0f7f036ae2da5286a13e99cf
wangbo <1186644190wxb@gmail.com>
2022-02-11 15:02:01 +0800

    add

---
+++ middle-software/rockrtmq/src/main/java/org/icu/rocketmq/Consumer.java
@@ -0,0 +1,44 @@
+package org.icu.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer {
+
+    public static void main(String[] args) throws MQClientException, MQClientException {
+
+        //谁来收？消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
+        //收哪儿的消息？命名中心获取消息服务器地址
+        consumer.setNamesrvAddr("localhost:9876");
+        //监听哪那个消息队列？
+        consumer.subscribe("topic1","*");
+        //处理业务流程
+        //注册监听器
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+
+                //写业务逻辑
+                //list中就说发送的消息
+                for (MessageExt messageExt : list) {
+
+                    System.out.println(messageExt);
+                    System.out.println(new String(messageExt.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        //启动消费者
+        consumer.start();
+        System.out.println("消费者启动");
+        //不用关掉消费者
+    }
+}
---
+++ middle-software/rockrtmq/src/main/java/org/icu/rocketmq/Producer.java
@@ -0,0 +1,27 @@
+package org.icu.rocketmq;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+public class Producer {
+
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException, MQBrokerException, RemotingException {
+
+        // 1. 谁来发？生产者
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+        // 2. 发给谁？命名服务器
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
+        // 3. 怎么发？
+        // 4. 发什么？主题、标签、内容
+        SendResult send = producer.send(new Message("topic1", "tag1", "hello world".getBytes()));
+        // 5. 发的结果是什么？
+        System.out.println(send);
+        // 6. 关闭生产者
+        producer.shutdown();
+    }
+}
